591565d0e53aecd3f1709e727f0035e4a3b299a9
cjh980402 <9804cjh@naver.com>
2022-02-04 01:19:48 +0900

    Solve two-pluses problem

--- HackerRank/Medium/two-pluses.cpp
+++ HackerRank/Medium/two-pluses.cpp
@@ -16,9 +16,9 @@ vector<string> split(const string &);
 int twoPluses(vector<string> grid)
 {
     int n = grid.size(), m = grid[0].size();
-    vector<vector<int>> maxPluses;
-    int maxK = 0;
+    vector<vector<int>> pluses;
 
+    // 플러스로 사용 가능한 모든 곳의 정보를 저장한다.
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
@@ -45,11 +45,7 @@ int twoPluses(vector<string> grid)
                         nowK++;
                 }
 
-                if (nowK >= maxK)
-                {
-                    maxK = nowK;
-                    maxPluses.push_back({i, j, nowK});
-                }
+                pluses.push_back({i, j, nowK});
             }
         }
     }
@@ -57,39 +53,53 @@ int twoPluses(vector<string> grid)
     int answer = 0;
     vector<int> maxP, second;
 
-    for (int i = maxPluses.size() - 1; i >= 1; i--)
+    sort(pluses.begin(), pluses.end(), [](const vector<int> &a, const vector<int> &b)
+         { return a[2] > b[2]; }); // 플러스의 길이에 따라 내림차순으로 정렬한다.
+
+    for (int i = 0; i < pluses.size() - 1; i++)
     {
-        maxP = maxPluses[i];
-        for (int k = -maxP[2]; k <= maxP[2]; k++)
+        maxP = pluses[i];
+        // maxC는 첫번째 플러스에서 감소할 길이의 값이다.
+        for (int maxC = 0; maxC <= maxP[2]; maxC++)
         {
-            grid[maxP[0] + k][maxP[1]] = 'U';
-            grid[maxP[0]][maxP[1] + k] = 'U';
-        }
-
-        for (int j = i - 1; j >= 0; j--)
-        {
-            bool isPossible = true;
-            second = maxPluses[j];
-
-            for (int k = -second[2]; isPossible && k <= second[2]; k++)
+            // maxP와 second가 겹치는지 확인하기 위해 maxP의 영역을 U로 표시한다.
+            for (int k = -(maxP[2] - maxC); k <= (maxP[2] - maxC); k++)
             {
-                if (grid[second[0] + k][second[1]] == 'U' || grid[second[0]][second[1] + k] == 'U')
+                grid[maxP[0] + k][maxP[1]] = 'U';
+                grid[maxP[0]][maxP[1] + k] = 'U';
+            }
+
+            for (int j = i + 1; j < pluses.size(); j++)
+            {
+                second = pluses[j];
+                // secondC는 두번째 플러스에서 감소할 길이의 값이다.
+                // maxC와 secondC를 사용하는 이유는 길이가 길어서 겹치는 경우가 발생할 때, 플러스의 일부분만 사용하면 문제에서 요구하는 최대의 넓이를 얻을 수 있는 경우가 있기 때문이다.
+                for (int secondC = 0; secondC <= second[2]; secondC++)
                 {
-                    isPossible = false;
+                    bool isPossible = true;
+                    for (int k = -(second[2] - secondC); isPossible && k <= (second[2] - secondC); k++)
+                    {
+                        // maxP와 second가 겹치는 경우는 유효하지 않은 경우이다.
+                        if (grid[second[0] + k][second[1]] == 'U' || grid[second[0]][second[1] + k] == 'U')
+                        {
+                            isPossible = false;
+                        }
+                    }
+
+                    // 현재 상태에서 maxP와 second가 겹치지 않으면 값을 확인한 후 갱신한다.
+                    if (isPossible && answer < (1 + 4 * (maxP[2] - maxC)) * (1 + 4 * (second[2] - secondC)))
+                    {
+                        answer = (1 + 4 * (maxP[2] - maxC)) * (1 + 4 * (second[2] - secondC));
+                    }
                 }
             }
 
-            if (isPossible)
-                break;
-        }
-
-        if (answer < (1 + 4 * maxP[2]) * (1 + 4 * second[2]))
-            answer = (1 + 4 * maxP[2]) * (1 + 4 * second[2]);
-
-        for (int k = -maxP[2]; k <= maxP[2]; k++)
-        {
-            grid[maxP[0] + k][maxP[1]] = 'G';
-            grid[maxP[0]][maxP[1] + k] = 'G';
+            // 현재 상태에 대한 검사가 끝났으므로 maxP의 영역을 원래 값으로 되돌린다.
+            for (int k = -(maxP[2] - maxC); k <= (maxP[2] - maxC); k++)
+            {
+                grid[maxP[0] + k][maxP[1]] = 'G';
+                grid[maxP[0]][maxP[1] + k] = 'G';
+            }
         }
     }
 
