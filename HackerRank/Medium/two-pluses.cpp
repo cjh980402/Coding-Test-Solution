b85ed5dfd995079dc4fe36decda85c8fdef73599
cjh980402 <9804cjh@naver.com>
2022-02-04 09:48:48 +0900

    Update two-pluses.cpp to better algorithm

--- HackerRank/Medium/two-pluses.cpp
+++ HackerRank/Medium/two-pluses.cpp
@@ -25,7 +25,8 @@ int twoPluses(vector<string> grid)
         {
             if (grid[i][j] == 'G')
             {
-                int nowK = 0;
+                // 겹치는 경우가 발생할 때, 플러스의 일부분만 사용해야 문제에서 요구하는 최대의 넓이를 얻을 수 있는 경우가 있기 때문에 0부터 최대길이까지 모두 저장한다.
+                pluses.push_back({i, j, 0});
 
                 for (int k = 1; i - k >= 0 && j - k >= 0 && i + k < n && j + k < m; k++)
                 {
@@ -42,64 +43,51 @@ int twoPluses(vector<string> grid)
                     if (count < 4)
                         break;
                     else
-                        nowK++;
+                        pluses.push_back({i, j, k});
                 }
-
-                pluses.push_back({i, j, nowK});
             }
         }
     }
 
     int answer = 0;
-    vector<int> maxP, second;
+    vector<int> first, second;
 
-    sort(pluses.begin(), pluses.end(), [](const vector<int> &a, const vector<int> &b)
-         { return a[2] > b[2]; }); // 플러스의 길이에 따라 내림차순으로 정렬한다.
-
+    // first와 second에 해당하는 모든 경우의 조합을 확인한다.
     for (int i = 0; i < pluses.size() - 1; i++)
     {
-        maxP = pluses[i];
-        // maxC는 첫번째 플러스에서 감소할 길이의 값이다.
-        for (int maxC = 0; maxC <= maxP[2]; maxC++)
+        first = pluses[i];
+        // first와 second가 겹치는지 확인하기 위해 first의 영역을 U로 표시한다.
+        for (int k = -first[2]; k <= first[2]; k++)
         {
-            // maxP와 second가 겹치는지 확인하기 위해 maxP의 영역을 U로 표시한다.
-            for (int k = -(maxP[2] - maxC); k <= (maxP[2] - maxC); k++)
-            {
-                grid[maxP[0] + k][maxP[1]] = 'U';
-                grid[maxP[0]][maxP[1] + k] = 'U';
-            }
+            grid[first[0] + k][first[1]] = 'U';
+            grid[first[0]][first[1] + k] = 'U';
+        }
 
-            for (int j = i + 1; j < pluses.size(); j++)
+        for (int j = i + 1; j < pluses.size(); j++)
+        {
+            second = pluses[j];
+            bool isPossible = true;
+            for (int k = -second[2]; isPossible && k <= second[2]; k++)
             {
-                second = pluses[j];
-                // secondC는 두번째 플러스에서 감소할 길이의 값이다.
-                // maxC와 secondC를 사용하는 이유는 길이가 길어서 겹치는 경우가 발생할 때, 플러스의 일부분만 사용하면 문제에서 요구하는 최대의 넓이를 얻을 수 있는 경우가 있기 때문이다.
-                for (int secondC = 0; secondC <= second[2]; secondC++)
+                // first와 second가 겹치는 경우는 유효하지 않은 경우이다.
+                if (grid[second[0] + k][second[1]] == 'U' || grid[second[0]][second[1] + k] == 'U')
                 {
-                    bool isPossible = true;
-                    for (int k = -(second[2] - secondC); isPossible && k <= (second[2] - secondC); k++)
-                    {
-                        // maxP와 second가 겹치는 경우는 유효하지 않은 경우이다.
-                        if (grid[second[0] + k][second[1]] == 'U' || grid[second[0]][second[1] + k] == 'U')
-                        {
-                            isPossible = false;
-                        }
-                    }
-
-                    // 현재 상태에서 maxP와 second가 겹치지 않으면 값을 확인한 후 갱신한다.
-                    if (isPossible && answer < (1 + 4 * (maxP[2] - maxC)) * (1 + 4 * (second[2] - secondC)))
-                    {
-                        answer = (1 + 4 * (maxP[2] - maxC)) * (1 + 4 * (second[2] - secondC));
-                    }
+                    isPossible = false;
                 }
             }
 
-            // 현재 상태에 대한 검사가 끝났으므로 maxP의 영역을 원래 값으로 되돌린다.
-            for (int k = -(maxP[2] - maxC); k <= (maxP[2] - maxC); k++)
+            // 현재 상태에서 first와 second가 겹치지 않으면 값을 확인한 후 갱신한다.
+            if (isPossible && answer < (1 + 4 * first[2]) * (1 + 4 * second[2]))
             {
-                grid[maxP[0] + k][maxP[1]] = 'G';
-                grid[maxP[0]][maxP[1] + k] = 'G';
+                answer = (1 + 4 * first[2]) * (1 + 4 * second[2]);
             }
+        }
+
+        // 현재 상태에 대한 검사가 끝났으므로 first의 영역을 원래 값으로 되돌린다.
+        for (int k = -first[2]; k <= first[2]; k++)
+        {
+            grid[first[0] + k][first[1]] = 'G';
+            grid[first[0]][first[1] + k] = 'G';
         }
     }
 
