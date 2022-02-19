a07985ddb92ab84f5eaa1bce37384692c478febf
cjh980402 <9804cjh@naver.com>
2022-02-19 23:51:48 +0900

    Add solution of 2022-02-19

--- HackerRank/Medium/billboards.cpp
+++ HackerRank/Medium/billboards.cpp
@@ -8,6 +8,7 @@ struct Profit
     int pos;
 };
 
+// 우선순위 큐에 비교함수로 사용하기 위해 ()연산자를 오버로딩한 클래스
 class CompareProfit
 {
 public:
@@ -36,32 +37,29 @@ long long billboards(int k, const vector<int> &revenue)
     }
 
     priority_queue<Profit, vector<Profit>, CompareProfit> pq; // value가 작은 값이 앞에 있음
+    // 우선순위 큐에는 전체에서 조건 k를 만족하기 위해 제거할 요소들의 합이 담긴다.
 
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i <= n; i++)
     {
         if (i <= k)
         {
-            pq.push({revenue[i], i});
+            pq.push({revenue[i], i}); // 각각 revenue[i]를 제외하는 경우에 대응된다.
         }
         else
         {
-            Profit p = pq.top();
-            while (p.pos < i - (k + 1))
+            // k+1 이내 거리에 있는 값이 작은 요소가 발견되면 반복을 멈춘다.
+            while (pq.top().pos < i - (k + 1))
             {
-                pq.pop();
-                p = pq.top();
+                pq.pop(); // 현재 위치 i에서 k+1보다 멀리 떨어진 값이 작은 요소들을 제거한다.
             }
-            pq.push({revenue[i] + p.value, i});
+            if (i < n)
+            {
+                pq.push({revenue[i] + pq.top().value, i}); // revenue[i]와 k+1 이내 거리에 있는 값이 작은 요소를 제외하는 경우에 대응된다.
+            }
         }
     }
 
-    Profit p = pq.top();
-    while (p.pos < n - (k + 1))
-    {
-        pq.pop();
-        p = pq.top();
-    }
-    return sum - p.value;
+    return sum - pq.top().value; // 제거할 요소들의 합 중 가장 작은 값을 뺀 값을 반환한다.
 }
 
 int main()
