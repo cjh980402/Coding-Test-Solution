7a41c6740ee1b7792787e22e42be6feb0e458c7b
cjh980402 <9804cjh@naver.com>
2022-03-20 23:40:54 +0900

    Update insertionsort1.cpp to one stage insertion sort

--- HackerRank/Easy/insertionsort1.cpp
+++ HackerRank/Easy/insertionsort1.cpp
@@ -18,32 +18,29 @@ void printArray(const vector<int> &arr)
  *  1. INTEGER n
  *  2. INTEGER_ARRAY arr
  */
-void insertionSort1(int n, vector<int>& arr)
+void insertionSort1(int n, vector<int> &arr)
 {
-    // 루프를 돌 때마다 i번째 값이 정렬된다.
+    // arr에서 마지막 원소만 정렬되지 않은 값이므로 해당 원소에 대해서만 삽입정렬 수행
+    int unsorted = arr[n - 1], bigThanUnsort = n - 1;
+
+    // 첫번째로 등장하는 현재 값보다 큰 값의 위치 찾기 -> 현재 값이 삽입될 위치
     for (int i = 0; i < n - 1; i++)
     {
-        int min = 1000000, minI = i;
-        // 최솟값과 위치 탐색
-        for (int j = i; j < n; j++)
+        if (arr[i] > unsorted)
         {
-            if (arr[j] < min)
-            {
-                min = arr[minI = j];
-            }
+            bigThanUnsort = i;
+            break;
         }
-        if (minI == i)
-            continue; // 현재 단계에서 최솟값이 맨 앞인 경우 이동 연산 생략 가능
+    }
 
-        for (int j = minI - 1; j >= i; j--)
-        {
-            arr[j + 1] = arr[j]; // 최솟값 앞에 있는 값들을 1칸씩 이동
-            printArray(arr);
-        }
-
-        arr[i] = min; // 최솟값을 맨 앞에 삽입
+    for (int i = n - 2; i >= bigThanUnsort; i--)
+    {
+        arr[i + 1] = arr[i]; // 넣을 위치 뒤에 있는 값들을 1칸씩 이동
         printArray(arr);
     }
+    arr[bigThanUnsort] = unsorted; // 현재 값을 올바른 위치에 삽입
+
+    printArray(arr);
 }
 
 int main()
